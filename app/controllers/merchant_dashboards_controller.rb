3c66cf05c073b47bd24254294651512b3eb16c04
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-12 21:20:06 -0800

    feat: added link to bulk_discounts index from merch_dash show page

--- app/controllers/merchant_dashboards_controller.rb
+++ app/controllers/merchant_dashboards_controller.rb
@@ -1,9 +1,7 @@
 class MerchantDashboardsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    
     @vip_customers = @merchant.top_customers
-
     @all_merchant_unshipped_items = @merchant.unshipped_items
   end
 end
